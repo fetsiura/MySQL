b05940330b213857df0fa3a04bb569594d489578
fetsiura <fetsiura@gmail.com>
2021-10-18 20:05:49 +0200

    some

--- src/main/java/pl/coderslab/mysql/create/Main04.java
+++ src/main/java/pl/coderslab/mysql/create/Main04.java
@@ -3,6 +3,30 @@ package pl.coderslab.mysql.create;
 public class Main04 {
 
     public static void main(String[] args) {
-
+        String str = "create table cinemas (\n" +
+                "    id int AUTO_INCREMENT primary key ,\n" +
+                "    name VARCHAR(255),\n" +
+                "    address VARCHAR(255)\n" +
+                ");\n" +
+                "\n" +
+                "create table movies(\n" +
+                "    id int auto_increment primary key ,\n" +
+                "    title VARCHAR(255),\n" +
+                "    description VARCHAR(255),\n" +
+                "    rating decimal(2)\n" +
+                ");\n" +
+                "\n" +
+                "create table tickets(\n" +
+                "    id int auto_increment primary key,\n" +
+                "    quantity int,\n" +
+                "    price decimal(2),\n" +
+                "    status TINYINT\n" +
+                ");\n" +
+                "\n" +
+                "create table payments (\n" +
+                "    id int auto_increment primary key ,\n" +
+                "    type VARCHAR(255),\n" +
+                "    payment_date DATE\n" +
+                ");";
     }
 }
