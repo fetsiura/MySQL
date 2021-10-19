c3efa9ff7cb2490eaa68a363e0dcfde5424f0887
fetsiura <fetsiura@gmail.com>
2021-10-19 20:31:41 +0200

    some

--- src/main/java/pl/coderslab/mysql/change/Main01.java
+++ src/main/java/pl/coderslab/mysql/change/Main01.java
@@ -1,5 +1,8 @@
 package pl.coderslab.mysql.change;
 
 public class Main01 {
-
+    String str = "alter table tickets add  priceUSD DECIMAL(10,2);\n" +
+            "alter table movies add director char(80);\n" +
+            "alter table movies modify director varchar(50);\n" +
+            "alter table tickets drop priceUSD;";
 }
--- src/main/java/pl/coderslab/mysql/change/Main02.java
+++ src/main/java/pl/coderslab/mysql/change/Main02.java
@@ -1,5 +1,13 @@
 package pl.coderslab.mysql.change;
 
 public class Main02 {
-
+    String str = "use cinemas_ex;\n" +
+            "\n" +
+            "alter table movies add watchCount  int;\n" +
+            "\n" +
+            "alter table movies add isTop tinyint default 0;\n" +
+            "\n" +
+            "alter table cinemas add openTime TIMESTAMP;\n" +
+            "alter table cinemas add closeTime TIMESTAMP;\n" +
+            "describe cinemas;";
 }
