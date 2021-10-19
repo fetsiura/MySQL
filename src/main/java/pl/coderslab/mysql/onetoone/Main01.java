62fd045b000246a77ba9d007df348aca806a0e38
fetsiura <fetsiura@gmail.com>
2021-10-19 21:32:14 +0200

    some

--- src/main/java/pl/coderslab/mysql/onetoone/Main01.java
+++ src/main/java/pl/coderslab/mysql/onetoone/Main01.java
@@ -1,5 +1,10 @@
 package pl.coderslab.mysql.onetoone;
 
 public class Main01 {
-
+ String str = "\n" +
+         "insert into  client_address( city, street, house_nr) VALUES ('New York', 'Main street', '22'),\n" +
+         "                                                            ('Warszawa','Motycka','23'),\n" +
+         "                                                            ('Las Vegas', 'Shevchenka', '34'),\n" +
+         "                                                            ('Kraków', 'Jana Pawła', '34'),\n" +
+         "                                                            ('Poznań', 'Januszki', '1');";
 }
