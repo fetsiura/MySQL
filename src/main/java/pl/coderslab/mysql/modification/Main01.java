4beb88691c01c67346f866d0a5acb9a412608b19
fetsiura <fetsiura@gmail.com>
2021-10-26 20:44:14 +0200

    some

--- src/main/java/pl/coderslab/mysql/modification/Main01.java
+++ src/main/java/pl/coderslab/mysql/modification/Main01.java
@@ -2,4 +2,14 @@ package pl.coderslab.mysql.modification;
 
 public class Main01 {
 
+    String str ="select count(*) from tickets;\n" +
+            "\n" +
+            "select type FROM payments group by type;\n" +
+            "select * from payments;\n" +
+            "select AVG(price) from tickets;\n" +
+            "select * from payments limit 2,5;\n" +
+            "select  type, count(*) as size from payments group by type having size >1;\n" +
+            "\n" +
+            "select SUM(quantity) from tickets where price > 23.15";
+
 }
--- src/main/java/pl/coderslab/mysql/modification/Main02.java
+++ src/main/java/pl/coderslab/mysql/modification/Main02.java
@@ -2,4 +2,10 @@ package pl.coderslab.mysql.modification;
 
 public class Main02 {
 
+    String str ="select * from movies where title like 'D%';\n" +
+            "select * from tickets where price > 15.30;\n" +
+            "select * from tickets where quantity >3;\n" +
+            "select * from movies where rating > 6.5;\n" +
+            "select * from movies order by rating desc limit 1;";
+
 }
