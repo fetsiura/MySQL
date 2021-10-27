d5ffa7c6bacb6f311b9e0d2262f17d9beadb4bb5
fetsiura <fetsiura@gmail.com>
2021-10-27 21:21:30 +0200

    some

--- src/main/java/pl/coderslab/mysql/onetomany/Main01.java
+++ src/main/java/pl/coderslab/mysql/onetomany/Main01.java
@@ -1,5 +1,19 @@
 package pl.coderslab.mysql.onetomany;
 
 public class Main01 {
-
+    String str = "create table opinions (\n" +
+            "    id int auto_increment ,\n" +
+            "    product_id int,\n" +
+            "    description VARCHAR(80),\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (product_id)\n" +
+            "                      REFERENCES products(id)\n" +
+            ");\n" +
+            "\n" +
+            "select * from products;\n" +
+            "select * from opinions;\n" +
+            "\n" +
+            "insert into products VALUES (2,'Juice', 'orange', 5.99);\n" +
+            "\n" +
+            "INSERT into opinions VALUES(3,2,'middle');\n";
 }
--- src/main/java/pl/coderslab/mysql/onetomany/Main02.java
+++ src/main/java/pl/coderslab/mysql/onetomany/Main02.java
@@ -2,4 +2,17 @@ package pl.coderslab.mysql.onetomany;
 
 public class Main02 {
 
+    String str = "select * from categories;\n" +
+            "select * from categories_sub;\n" +
+            "create table categories (\n" +
+            "    id int auto_increment primary key ,\n" +
+            "    name VARCHAR(80)\n" +
+            ");\n" +
+            "\n" +
+            "create table categories_sub (\n" +
+            "    id int auto_increment primary key ,\n" +
+            "    main_id int REFERENCES categories(id),\n" +
+            "    name varchar(80)\n" +
+            ");";
+
 }
