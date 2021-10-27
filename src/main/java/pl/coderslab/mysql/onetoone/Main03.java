53628d3e24cf09563f1fadfe80965fae6b25d277
fetsiura <fetsiura@gmail.com>
2021-10-27 18:59:00 +0200

    some

--- src/main/java/pl/coderslab/mysql/onetoone/Main03.java
+++ src/main/java/pl/coderslab/mysql/onetoone/Main03.java
@@ -2,4 +2,7 @@ package pl.coderslab.mysql.onetoone;
 
 public class Main03 {
 
+    String str = "ALTER TABLE payments add constraint foreign key (id) REFERENCES tickets(id);\n" +
+            "INSERT into payments (id, type, payment_date) VALUES (12, 'cash','2021-12-31');";
+
 }
