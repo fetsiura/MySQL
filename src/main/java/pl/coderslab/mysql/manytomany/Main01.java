9451a838f8ea2714e93850c9efc54469eb3b1f52
fetsiura <fetsiura@gmail.com>
2021-10-28 17:55:13 +0200

    some

--- src/main/java/pl/coderslab/mysql/manytomany/Main01.java
+++ src/main/java/pl/coderslab/mysql/manytomany/Main01.java
@@ -1,5 +1,13 @@
 package pl.coderslab.mysql.manytomany;
 
 public class Main01 {
-
+String str = "create table products_orders(\n" +
+        "    id int auto_increment primary key ,\n" +
+        "    product_id int not null,\n" +
+        "    order_id int not null ,\n" +
+        "    FOREIGN KEY (product_id) REFERENCES products(id),\n" +
+        "    FOREIGN KEY (order_id) REFERENCES orders(id)\n" +
+        ");\n" +
+        "\n" +
+        "insert into products_orders(product_id, order_id) VALUES (1,2),(2,2);";
 }
--- src/main/java/pl/coderslab/mysql/manytomany/Main02.java
+++ src/main/java/pl/coderslab/mysql/manytomany/Main02.java
@@ -1,5 +1,13 @@
 package pl.coderslab.mysql.manytomany;
 
 public class Main02 {
-
+String str ="create table cinemas_movies (\n" +
+        "    id int auto_increment primary key ,\n" +
+        "    cinema_id int not null,\n" +
+        "    movie_id int not null ,\n" +
+        "    FOREIGN KEY (cinema_id) REFERENCES cinemas(id),\n" +
+        "    FOREIGN KEY (movie_id) REFERENCES movies(id)\n" +
+        ");\n" +
+        "\n" +
+        "insert into cinemas_movies( cinema_id, movie_id) VALUES (1,1),(1,2),(1,3),(2,5),(2,6);";
 }
