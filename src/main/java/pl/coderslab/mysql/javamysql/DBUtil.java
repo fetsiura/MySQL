37a316d35e1b609f10a0218bb322d02a080d38ca
fetsiura <fetsiura@gmail.com>
2021-10-29 22:37:02 +0200

    some

--- src/main/java/pl/coderslab/mysql/javamysql/DBUtil.java
+++ src/main/java/pl/coderslab/mysql/javamysql/DBUtil.java
@@ -1,5 +1,16 @@
 package pl.coderslab.mysql.javamysql;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBUtil {
+
+    public static String DB_URL = "jdbc:mysql://localhost:3306/cinemas_ex?serverTimezone=UTC&useSSL=false";
+    public static String DB_USER = "root";
+    public static String DB_PASS = "coderslab";
+
+    public static Connection connect() throws SQLException {}
+
+
 
 }
